256a74b523ad620666dfe13b7d959fe3053cdb2b
TOMProject <yeweigangemail@163.com>
2018-12-13 18:13:27 +0800

    springCloud 第一次提交

---
+++ eureka-server/src/main/java/org/spring/server/EurekaServerApplication.java
@@ -0,0 +1,20 @@
+package org.spring.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * 注册一个服务中心
+ * @author 
+ *
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run( EurekaServerApplication.class, args );
+    }
+}
+
